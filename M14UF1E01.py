5145d28b5d1dac171bc06a1adf4e07079884389d
cf19marcventura <71392013+cf19marcventura@users.noreply.github.com>
2020-11-04 18:54:25 +0100

    Update M14UF1E01.py

--- M14UF1E01.py
+++ M14UF1E01.py
@@ -1,9 +1,6 @@
-print("2")
-print(2)
-print(0x123)
-print(0o123)
-print ( "M'agrada \"Monty Python\"")
-print('Me gusta "Monty Python"')
-#Comentari de una sola linea
-"""Comentari
-multilinea"""
+print("2") #literal cadena
+print(2) #literal sencer
+print(0x123) #literal sencer hexadecimal
+print(0o123) #literal sencer octal
+print ( "M'agrada \"Monty Python\"") #utilitzem tecla escapament per comilla doble
+print('Me gusta "Monty Python"') 
